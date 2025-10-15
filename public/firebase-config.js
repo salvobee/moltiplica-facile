ecd59137ec87595807e093d3ebab3e05426e8288
Salvo Bonanno <salvob.labs@gmail.com>
2025-10-15 11:37:42 +0200

    Adds firebase configuration

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,11 @@
 // Sostituisci i valori di esempio con le credenziali del tuo progetto Firebase.
 // Questo file viene servito così com'è anche su Firebase Hosting: non contiene segreti.
 window.__FIREBASE_CONFIG__ = {
-  apiKey: "INSERISCI_FIREBASE_API_KEY",
-  authDomain: "il-tuo-progetto.firebaseapp.com",
-  projectId: "il-tuo-progetto",
-  storageBucket: "il-tuo-progetto.firebasestorage.app",
-  appId: "INSERISCI_FIREBASE_APP_ID",
-  // measurementId: "OPZIONALE_GA4_ID",
+    apiKey: "AIzaSyDYmj1YjvxCFkgBqHrll7n9HMqigsQ8Y30",
+    authDomain: "moltiplica-facile.firebaseapp.com",
+    projectId: "moltiplica-facile",
+    storageBucket: "moltiplica-facile.firebasestorage.app",
+    messagingSenderId: "587180894717",
+    appId: "1:587180894717:web:f7418ebbf5eba0a1559194",
+    measurementId: "G-DJ07561K72"
 };
